b3382587f6910c96ee05fe02c7447b9c27e56298
Esa Kataja <ekataja@gmail.com>
2021-01-17 18:59:51 +0200

    makedist changes

--- makedist.sh
+++ makedist.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-version=$(git tag)
+version=$(git describe)
 rev=$(git rev-parse --short HEAD)
 
+echo "Creating Directories\n"
 mkdir -p project/build/linux
 mkdir -p project/build/win64
 mkdir -p dist
 
+echo "Exporting projects\n"
 godot project/project.godot --export-debug linux build/linux/HCSpace.x86_64
 godot project/project.godot --export-debug win64 build/win64/HCSpace.exe
 
+echo "Compressing packages\n"
 cd project/build/
 zip -r HCSpace-win64-$version\_$rev.zip win64/
 tar cjf HCSpace-linux-$version\_$rev.tar.bz2 linux/
@@ -18,5 +21,6 @@ rm -rf linux/*
 mv *.zip ../../dist
 mv *.bz2 ../../dist
 cd ../../dist
+rm HCSpace-win64-latest.zip HCSpace-linux-latest.tar.bz2
 ln HCSpace-win64-$version\_$rev.zip HCSpace-win64-latest.zip
 ln HCSpace-linux-$version\_$rev.tar.bz2 HCSpace-linux-latest.tar.bz2
